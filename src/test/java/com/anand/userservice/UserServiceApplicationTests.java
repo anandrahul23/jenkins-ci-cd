31e47a36150fc75ef834b1a9aab7f4120848de5d
Rahul Anand <anand.rahul2886@gmail.com>
2023-11-04 22:28:27 +0000

    demo unit test case

--- src/test/java/com/anand/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/anand/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,19 @@
 package com.anand.userservice;
 
+import com.anand.userservice.users.dto.DisplayUserDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class UserServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void testCreateDisplayUserDTO()
+    {
+        var displayUser = new DisplayUserDto();
+        assertNotNull(displayUser);
+    }
 
 }
